d85ffa7a1cfa7158d91d878567a9f223c0c65db0
Machuka Joy <machukajoy@gmail.com>
2022-05-30 18:57:38 +0300

    ts: tests the location and image instanciation

--- gallery/tests.py
+++ gallery/tests.py
@@ -3,9 +3,18 @@ from .models import Photos,Location,Category
 
 # Create your tests here.
 
-class PhotosTestClass(TestCase):
+class LocationTestClass(TestCase):
+  def setUp(self):
+    self.Kilifi = Location(location_name='Kilifi')
 
-    # set up method
-    def setUp(self):
-        self.photo_location = Location(location_name = 'Nairobi')
-        self.photo_location.save()
+  def test_instance(self):
+    self.assertTrue(isinstance(self.Kilifi, Location))
+
+  def test_save_method(self):
+    self.Kilifi.save_location_name()
+
+class PhotosTestClass(TestCase):
+  def setUp(self):
+    self.car_photo = Photos(photo_name ='mum' , photo_description= 'My mother' , photo_location ='Kilifi' photo_category ='family' photo ='image.jpeg')
+
+    self.new_photo = photo(name = )
