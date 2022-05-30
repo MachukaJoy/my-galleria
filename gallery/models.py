544e5a37788cec31a80d9038507ede8206409939
Machuka Joy <machukajoy@gmail.com>
2022-05-30 19:45:10 +0300

    ts: test save image

--- gallery/models.py
+++ gallery/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Photos(models.Model):
     photo_name =models.CharField(max_length = 30)
     photo_description = models.TextField()
-    Photo = models.ImageField(upload_to = 'images/', default='image.jpg')
+    photo = models.ImageField(upload_to = 'images/', default='image.jpg')
     photo_location = models.ForeignKey('Location', on_delete=models.SET_NULL, default = '', null=True)
     photo_category = models.ForeignKey('Category', on_delete=models.CASCADE, default='')
 
@@ -17,6 +17,12 @@ class Location(models.Model):
 
     def __str__(self):
         return self.location_name
+
+    def save_location_name(self):
+        self.save()
+
+    def delete_location_name(self):
+        self.delete()
 
 
 class Category(models.Model):
--- gallery/tests.py
+++ gallery/tests.py
@@ -1,20 +1,45 @@
 from django.test import TestCase
-from .models import Photos,Location,Category
+from .models import Photos, Location, Category
 
 # Create your tests here.
 
 class LocationTestClass(TestCase):
   def setUp(self):
     self.Kilifi = Location(location_name='Kilifi')
+    self.Kilifi.save_location_name()
 
   def test_instance(self):
     self.assertTrue(isinstance(self.Kilifi, Location))
 
   def test_save_method(self):
     self.Kilifi.save_location_name()
+    Locations = Location.objects.all()
+    print(Locations)
+    self.assertTrue(len(Locations)==1)
+
+  def test_delete_method(self):
+    self.Kilifi.delete_location_name()
+    Locations = Location.objects.all()
+    print(Locations)
+    self.assertTrue(len(Locations)==0)
+
+
+  def tearDown(self):
+    Location.objects.all().delete()
 
 class PhotosTestClass(TestCase):
   def setUp(self):
-    self.car_photo = Photos(photo_name ='mum' , photo_description= 'My mother' , photo_location ='Kilifi' photo_category ='family' photo ='image.jpeg')
+    self.new_location = Location(location_name = 'Mombasa')
+    self.new_location.save()
 
-    self.new_photo = photo(name = )
+    self.new_category = Category(category_name = 'Adventure')
+    self.new_category.save()
+
+    self.new_pic = Photos(photo_name = 'Mum', photo_description = 'Mama bear', photo_location = self.new_location, photo_category = self.new_category, photo = 'image.jpg' )
+    self.new_pic.save()
+
+  def test_instance(self):
+    self.assertTrue(isinstance(self.new_pic, Photos))
+
+  def pic(self):
+    self.new_pic.save_image()
