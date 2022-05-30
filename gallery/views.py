a480ee06a5660b1cb4be55ce45b238237ad534c5
Machuka Joy <machukajoy@gmail.com>
2022-05-30 20:19:18 +0300

    ft: add method to render photos

--- gallery/views.py
+++ gallery/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render, redirect
 from django.http  import HttpResponse
+from .models import Photos
 
 
 # Create your views here.
 def home(request):
   return render(request,'home.html')
+
+def photos(request):
+  photo = Photos.objects.all()
+  return render(request, 'photo.html', {"photo" : photo})
