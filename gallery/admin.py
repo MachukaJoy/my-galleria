bc217011fd5a6e2866021bafd54953e3bc50cf8b
Machuka Joy <machukajoy@gmail.com>
2022-05-31 21:25:23 +0300

    ft: adds admin url

--- gallery/admin.py
+++ gallery/admin.py
@@ -3,3 +3,7 @@ from .models import Photos,Location,Category
 
 
 # Register your models here.
+# Register your models here.
+admin.site.register(Photos)
+admin.site.register(Location)
+admin.site.register(Category)
